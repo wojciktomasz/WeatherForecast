d20992bc28764b57822f240e5e4778b4ca719b23
Tomasz Wojcik <Superstar@MacBook-Pro-Tomasz.local>
2017-12-08 23:43:52 +0100

    Google Maps api key added

--- src/components/GoogleMaps.js
+++ src/components/GoogleMaps.js
@@ -2,17 +2,12 @@
  * Created by Superstar on 08.12.2017.
  */
 import React from 'react'
-import { GoogleMapLoader, GoogleMap } from 'react-google-maps'
+import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps'
 
-const GoogleMaps = (props) => {
+const GoogleMaps = withGoogleMap((props) => {
     return (
-        <GoogleMapLoader
-            containerElement={ <div style={{height: '100%'}} />}
-            googleMapElement={
-                <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
-            }
-        />
+        <GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat, lng: props.lon}} />
     )
-}
+})
 
 export default GoogleMaps
